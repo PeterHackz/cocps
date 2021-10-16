e614dbf4a20db53e8c59032684116f23eb35926e
SB-9838 <86655602+SB-9838@users.noreply.github.com>
2021-10-16 10:01:38 +0300

    Create Player.py

---
+++ Logic/Player.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+class Player:
+
+    HighID = 0
+    LowID = 0
+    Token = None
+
+    def __init__(self, device):
+        self.device = device
+
+    def encode(self):
+        return None
